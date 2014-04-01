619e7505d5a1cf707761a66ae925d425db7d64f5
deepakparyani <deepakparyani123@gmail.com>
2014-04-01 22:42:59 +0530

    created the project structure
    added required files to get started and project hierarchy.

---
+++ js/app.js
@@ -0,0 +1,23 @@
+/*global $*/
+define(
+  ['marionette','vent'],
+  function(marionette, vent){
+    "use strict";
+
+    var app = new marionette.Application(),
+
+    app.addRegions({
+      header : '#header',
+      main   : '#main',
+      footer : '#footer'
+    });
+
+    app.addInitializer(function(){
+      app.header.show(new Header(viewOptions));
+      app.main.show(new TodoListCompositeView(viewOptions));
+      app.footer.show(new Footer(viewOptions));
+    });
+    return app;
+
+  }
+);
