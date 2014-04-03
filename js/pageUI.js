171e52706e94aabdcdc726393e7de50926e10a4d
deepakparyani <deepakparyani123@gmail.com>
2014-04-03 14:14:16 +0530

    changed the color of navbar
    still, navbar not working as expected

--- js/pageUI.js
+++ js/pageUI.js
@@ -5,12 +5,14 @@
                 if (scrollTop > 48) {
 				
                     $('#nav').addClass('getFixed');
-                   
+                   //$('#top').hide();
+				   //$('#nav').show();
                     $('body').addClass('header-fixed');
                     //hideFilters();
                 } else {
                     if (scrollTop < 48) {
                         $('#nav').removeClass('getFixed');
+						  // $('#top').show();
                     }
                     $('body').removeClass('header-fixed');
                 }
